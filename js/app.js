f3f6d37108a88d72790233d5d6f9c6bdee4d74cf
samtopper <sakhancse@gmail.com>
2016-12-01 14:54:56 +0530

    initial push

---
+++ js/app.js
@@ -0,0 +1,6 @@
+ $.ajax({
+        url:     ,         // here mention comma at the end, not semicolon.
+        dataType: 'jsonp',
+    }).done(function(res){      // using .done() instead of success:
+
+    });
